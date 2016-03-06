8efe5cbd4441be5290c894d1edb9d46be3f7c325
pooja <pooja.sainani7@gmail.com>
2016-03-06 17:40:04 +0530

    added products lis and categories; change gradle build and manifest

---
+++ ShopKaro/app/src/main/java/cfh/com/shopkaro/MyOrderServiceItemRecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package cfh.com.shopkaro;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cfh.com.shopkaro.MyOrderService.OnListFragmentInteractionListener;
+import cfh.com.shopkaro.dummy.OrderServiceContent;
+import cfh.com.shopkaro.dummy.OrderServiceContent.DummyItem;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyOrderServiceItemRecyclerViewAdapter extends RecyclerView.Adapter<MyOrderServiceItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyOrderServiceItemRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_orderserviceitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mService.setText(mValues.get(position).service);
+        holder.mDetails.setText(mValues.get(position).details);
+        holder.mDescription.setText(mValues.get(position).description);
+        holder.mLocation.setText(mValues.get(position).location);
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mService;
+        public final TextView mDescription;
+        public final TextView mDetails;
+        public final TextView mLocation;
+        public OrderServiceContent.DummyItem mItem;
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mService = (TextView) view.findViewById(R.id.item_Servicename);
+            mDescription = (TextView) view.findViewById(R.id.item_ServiceDescription);
+            mDetails = (TextView) view.findViewById(R.id.item_SellerDetails);
+            mLocation = (TextView) view.findViewById(R.id.item_Location);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mService.getText() + "'";
+        }
+    }
+}
--- ShopKaro/app/src/main/java/cfh/com/shopkaro/OfferedByMeActivity.java
+++ ShopKaro/app/src/main/java/cfh/com/shopkaro/OfferedByMeActivity.java
@@ -1,6 +1,7 @@
 package cfh.com.shopkaro;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -16,14 +17,6 @@ import android.widget.TextView;
 
 public class OfferedByMeActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
@@ -42,9 +35,14 @@ public class OfferedByMeActivity extends AppCompatActivity {
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
+
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
 
     }
 
